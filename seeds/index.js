8ceda7be79f665e4f52a08d19287d9f132470572
Kaushal Kahapola <kaushal.21@cse.mrt.ac.lk>
2024-01-13 20:10:59 +0530

    style edit page

--- seeds/index.js
+++ seeds/index.js
@@ -22,10 +22,12 @@ const seedDB = async () => {
     await Campground.deleteMany({})
     for (let i = 0; i < 50; i++) {
         const price = Math.floor(Math.random()*20 ) +10;
+        // const randomImageNumber = Math.floor(Math.random() * 1000); // or any other suitable range
         const camp = new Campground({
             location: `${sample(cities).city}, ${sample(cities).state}`,
             title: `${sample(descriptors)} ${sample(places)}`,
-            image:`http://source.unsplash.com/collection/484351`,
+            image: `http://source.unsplash.com/collection/484351`,
+            // image: `http://source.unsplash.com/collection/484351/?sig=${randomImageNumber}`,
             price: price,
             description:   'Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim aliquam recusandae vitae, exercitationem placeat magnam fugit doloribus quo necessitatibus error provident alias voluptas ipsa quae mollitia debitis vero eum quam?'
         })
