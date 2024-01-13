865d969674e5551aa23be10702e9325991dc469f
Kaushal Kahapola <kaushal.21@cse.mrt.ac.lk>
2024-01-13 12:07:42 +0530

    add a ejsMate layout - simple before after

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const { log } = require('console');
 const express = require('express');
 const path = require('path');
+const ejsMate = require('ejs-mate')
 const mongoose = require('mongoose');
 const methodOverride = require('method-override')
 const Campground = require('./models/campground');
@@ -19,6 +20,7 @@ db.once('open', () => {
 
 const app = express()
 
+app.engine('ejs', ejsMate)
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, 'views'))
 
