73e129aadac8310fda44cff6ead6e747972f6e75
Kaushal Kahapola <kaushal.21@cse.mrt.ac.lk>
2024-01-15 23:36:52 +0530

    add express-session and cookie

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const { log } = require('console');
 const express = require('express');
+const session = require('express-session')
 const path = require('path');
 const ejsMate = require('ejs-mate')
 const mongoose = require('mongoose');
@@ -9,9 +10,6 @@ const campgrounds = require('./routes/campgrounds.js')
 const reviews = require('./routes/reviews.js')
 
 mongoose.connect('mongodb://localhost:27017/yelp-camp', {
-    useNewUrlParser: true,
-    // useCreateIndex: true,  // Correct option name
-    useUnifiedTopology: true
 });
 
 const db = mongoose.connection
@@ -29,6 +27,20 @@ app.set('views', path.join(__dirname, 'views'))
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'))
 app.use(express.static(path.join(__dirname, 'public')))
+
+const sessionConfig = {
+    secret: 'thisisnotgood',
+    resave: false,
+    saveUninitialized: true,
+    cookie:{
+        httpOnly: true,
+        expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
+        maxAge: 1000 * 60 * 60 * 24 * 7,
+
+    }
+}
+
+app.use(session(sessionConfig))
 
 
 app.use('/campgrounds',campgrounds)
